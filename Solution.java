f4489cf64e8cc8ecb8a5109bf9a7b4cfdae97b39
Valix93 <v.lipari@studenti.unina.it>
2021-04-17 18:59:57 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,3 +1,7 @@
+/* 
+ * Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. 
+ * Then print the respective minimum and maximum values as a single line of two space-separated long integers. 
+ */ 
 import java.io.*;
 import java.math.*;
 import java.security.*;
