11aa613b3aac8a9e28f11d65ee514521c23f16ab
Valix93 <v.lipari@studenti.unina.it>
2021-04-18 18:17:02 +0200

    Update Solutions.sql

--- SQL/Solutions.sql
+++ SQL/Solutions.sql
@@ -172,3 +172,11 @@ Query a count of the number of cities in CITY having a Population larger than 10
 SELECT COUNT(NAME)
 FROM CITY
 WHERE POPULATION>100000;
+
+/*
+Revising Aggregations - The Sum Function
+Query the total population of all cities in CITY where District is California.
+*/
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE DISTRICT LIKE 'California';
