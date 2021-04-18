35e9bab0fd312ef9c5f272845113457869a411c0
Valix93 <v.lipari@studenti.unina.it>
2021-04-18 17:04:47 +0200

    Update Solutions.sql

--- SQL/Solutions.sql
+++ SQL/Solutions.sql
@@ -61,6 +61,20 @@ SELECT (COUNT(CITY) - COUNT(DISTINCT CITY))
 FROM STATION
                             
 /*
+Weather Observation Station 5
+Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). 
+If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+*/
+SELECT CITY, LENGTH(CITY)
+FROM STATION
+WHERE LENGTH(CITY) = (SELECT MIN(LENGTH(CITY)) FROM STATION)
+ORDER BY CITY ASC LIMIT 1;
+SELECT CITY, LENGTH(CITY)
+FROM STATION
+WHERE LENGTH(CITY) = (SELECT MAX(LENGTH(CITY)) FROM STATION);
+
+                            
+/*
 Weather Observation Station 11
 Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
 */
