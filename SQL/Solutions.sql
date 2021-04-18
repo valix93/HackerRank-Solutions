53e6b460ded83ac7e25f4fc8e69a71ed9040b9a4
Valix93 <v.lipari@studenti.unina.it>
2021-04-18 19:52:14 +0200

    Update Solutions.sql

--- SQL/Solutions.sql
+++ SQL/Solutions.sql
@@ -177,6 +177,14 @@ Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 3
 SELECT ROUND(MIN(LAT_N),4)
 FROM STATION
 WHERE LAT_N > 38.7780
+
+/*
+Weather Observation Station 17
+Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than 38.7780. Round your answer to 4 decimal places.
+*/
+SELECT ROUND(LONG_W,4)
+FROM STATION
+WHERE LAT_N = (SELECT MIN(LAT_N) FROM STATION WHERE LAT_N>38.7780)
                                  
 /*
 Employee Names
