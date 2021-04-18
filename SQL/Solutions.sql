9611228e46cb2d3f333dd95003e98d0696d75118
Valix93 <v.lipari@studenti.unina.it>
2021-04-18 19:39:40 +0200

    Update Solutions.sql

--- SQL/Solutions.sql
+++ SQL/Solutions.sql
@@ -145,11 +145,21 @@ WHERE CITY REGEXP '^[^aeiou].*[^aeiou]$';
 
 /*
 Weather Observation Station 13
-Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. Truncate your answer to 4 decimal places.
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. 
+Truncate your answer to 4 decimal places.
 */
 SELECT ROUND(SUM(LAT_N),4)
 FROM STATION
 WHERE LAT_N > 38.7880 AND LAT_N < 137.2345;
+                                 
+/*
+Weather Observation Station 14
+Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345.
+Truncate your answer to 4 decimal places.
+*/
+SELECT ROUND(MAX(LAT_N),4)
+FROM STATION
+WHERE LAT_N < 137.2345;
                                  
 /*
 Employee Names
