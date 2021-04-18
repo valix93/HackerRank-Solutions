3451df8506c1d60fd108e645a0a2f208915f8ccf
Valix93 <v.lipari@studenti.unina.it>
2021-04-18 19:46:37 +0200

    Update Solutions.sql

--- SQL/Solutions.sql
+++ SQL/Solutions.sql
@@ -160,6 +160,15 @@ Truncate your answer to 4 decimal places.
 SELECT ROUND(MAX(LAT_N),4)
 FROM STATION
 WHERE LAT_N < 137.2345;
+
+/*
+Weather Observation Station 15
+Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345.
+Truncate your answer to 4 decimal places.
+*/
+SELECT ROUND(LONG_W,4)
+FROM STATION
+WHERE LAT_N = (SELECT MAX(LAT_N) FROM STATION WHERE LAT_N < 137.2345);
                                  
 /*
 Employee Names
