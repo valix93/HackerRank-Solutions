372587a5750aa1b1a3f430d077d47469c849b14c
Valix93 <v.lipari@studenti.unina.it>
2021-04-18 19:35:47 +0200

    Update Solutions.sql

--- SQL/Solutions.sql
+++ SQL/Solutions.sql
@@ -144,6 +144,14 @@ SELECT DISTINCT CITY FROM STATION
 WHERE CITY REGEXP '^[^aeiou].*[^aeiou]$';
 
 /*
+Weather Observation Station 13
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. Truncate your answer to 4 decimal places.
+*/
+SELECT ROUND(SUM(LAT_N),4)
+FROM STATION
+WHERE LAT_N > 38.7880 AND LAT_N < 137.2345;
+                                 
+/*
 Employee Names
 Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
 */
