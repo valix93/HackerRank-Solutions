d84bbd2bbd610942e802ddf6fb00fc5cd3dd753c
Valix93 <v.lipari@studenti.unina.it>
2021-04-18 17:17:07 +0200

    Update Solutions.sql

--- SQL/Solutions.sql
+++ SQL/Solutions.sql
@@ -86,6 +86,14 @@ Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from 
 SELECT DISTINCT CITY
 FROM STATION
 WHERE CITY REGEXP '^[aeiou]';
+                                 
+/*
+Weather Observation Station 7
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP '[aeiou]$';
                             
 /*
 Weather Observation Station 11
