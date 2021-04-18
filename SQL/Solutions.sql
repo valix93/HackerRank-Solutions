1cf02b8dc8b1a9c36806a0a05ca397101519d26b
Valix93 <v.lipari@studenti.unina.it>
2021-04-18 18:25:52 +0200

    Update Solutions.sql

--- SQL/Solutions.sql
+++ SQL/Solutions.sql
@@ -188,3 +188,10 @@ Query the average population of all cities in CITY where District is California.
 SELECT AVG(POPULATION)
 FROM CITY
 WHERE DISTRICT LIKE 'California'
+
+/*
+Average Population
+Query the average population for all cities in CITY, rounded down to the nearest integer.
+*/
+SELECT ROUND(AVG(POPULATION))
+FROM CITY
