64631d2a714bda7e75bb08632b9083e5a4281cbf
Valix93 <v.lipari@studenti.unina.it>
2021-04-18 16:05:21 +0200

    Update Solutions.sql

--- SQL/Solutions.sql
+++ SQL/Solutions.sql
@@ -6,6 +6,13 @@ SELECT *
 FROM CITY
 WHERE POPULATION > 100000 AND COUNTRYCODE LIKE 'USA';
 
+/*
+Revising the Select Query II
+Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+*/
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE LIKE 'USA' and POPULATION > 120000
 
 /*
 Weather Observation Station 11
