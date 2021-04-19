26fe97c399bb80de5ee511627d804b305a31985e
Valix93 <v.lipari@studenti.unina.it>
2021-04-19 20:02:15 +0200

    Update Solutions.sql

--- SQL/Solutions.sql
+++ SQL/Solutions.sql
@@ -326,8 +326,29 @@ ORDER BY RIGHT(NAME,3), ID;
                                     
 /*
 Draw The Triangle 1
-P(R) represents a pattern drawn by Julia in R rows.
+P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
 Write a query to print the pattern P(20).
 */
 set @r=21;
-select repeat('* ', @r := @r - 1) from information_schema.tables limit 20;
+SELECT REPEAT('* ', @r := @r - 1) 
+FROM information_schema.TABLES 
+LIMIT 20;
+                                    
+/*
+P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
+*
+**
+***
+****
+*****
+Write a query to print the pattern P(20).
+*/
+set @r=0;
+SELECT REPEAT('* ', @r := @r+1)
+FROM information_schema.tables
+LIMIT 20
