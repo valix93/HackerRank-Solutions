a19b91f9448692c209e3ea94d30c113238f39ec0
Valix93 <v.lipari@studenti.unina.it>
2021-04-19 20:05:45 +0200

    Update Solutions.sql

--- SQL/Solutions.sql
+++ SQL/Solutions.sql
@@ -213,14 +213,15 @@ FROM EMPLOYEE
 WHERE SALARY > 2000 AND MONTHS < 10
 ORDER BY EMPLOYEE_ID ASC;
 
- /*
+/*
 Type of Triangle
 Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. 
 Output one of the following statements for each record in the table:
 Equilateral: It's a triangle with 3 sides of equal length.
 Isosceles: It's a triangle with 2 sides of equal length.
 Scalene: It's a triangle with 3 sides of differing lengths.
-Not A Triangle: The given values of A, B, and C don't form a triangle.*/
+Not A Triangle: The given values of A, B, and C don't form a triangle.
+*/
 SELECT IF(A+B>C AND A+C>B AND B+C>A, 
     IF(A=B AND B=C, 'Equilateral', 
     IF(A=B OR B=C OR A=C, 'Isosceles', 'Scalene')), 
@@ -351,4 +352,5 @@ Write a query to print the pattern P(20).
 set @r=0;
 SELECT REPEAT('* ', @r := @r+1)
 FROM information_schema.tables
-LIMIT 20
+LIMIT 20;
+
